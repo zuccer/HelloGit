8258f029060e1bba205a61b209ba25c9a8407143
heping <463132876@qq.com>
2015-10-29 11:33:53 +0800

    1.init project2

---
+++ src/cn/zucc/HelloPeace.java
@@ -0,0 +1,7 @@
+package cn.zucc;
+
+public class HelloPeace {
+	public static void main(String[] args) {
+		System.out.println("Hello Peace!");
+	}
+}
---
+++ src/cn/zucc/Hellohello.java
@@ -0,0 +1,7 @@
+package cn.zucc;
+
+public class Hellohello {
+	public static void main(String[] args) {
+		System.out.println(123);
+	}
+}
