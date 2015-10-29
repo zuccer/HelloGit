40a290f3b4a3a797b70d5d6feabc25dbd0ea16ba
zhengxiaofeng <1073075190@qq.com>
2015-10-29 11:17:42 +0800

    2 1

---
+++ src/cn/zucc/HelloGit.java
@@ -0,0 +1,11 @@
+package cn.zucc;
+
+import java.util.Scanner;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String name = input.next();
+		System.out.println(name);
+	}
+}
